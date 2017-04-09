6cf0bfe37d6ccdeab2702d1ea0cae79582ddefd9
Jorge Perales Diaz <slipknot_jpd@hotmail.com>
2017-04-09 14:18:34 -0500

    AzureTaskList
    The Azure Mobile App Sample project for Xamarin Forms allows to get and
    post information with the NuGet Microsoft.WindowsAzure.MobileServices

---
+++ TaskList/TaskList/TaskList.Android/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace TaskList.Droid
+{
+    [Activity (Label = "TaskList.Droid",
+        Icon = "@drawable/icon",
+        Theme="@android:style/Theme.Holo.Light",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+            // Initialize Azure Mobile Apps
+            Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+
+            // Initialize Xamarin Forms
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            // Load the main Application
+			LoadApplication(new TaskList.App());
+		}
+	}
+}
+
